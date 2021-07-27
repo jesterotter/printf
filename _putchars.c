3083f99b11a01e2d9e717f0bafb0c3c2c0d66b8d
jesterotter <mumbithe97@gmail.com>
2021-07-27 07:28:31 -0700

    fixed errors 2.3

--- _putchars.c
+++ _putchars.c
@@ -1,36 +1,12 @@
-#include "holberton.h"
+#include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: an arg_list containing the character to be printed
- * @format: the format to use to prepare the string
- * Return: On success 1.
- * On error, return -1
+ *_putchar - writes a character to stdout
+ *@c:charcter to print
+ *Return:1 - on success, -1 on error
+ *
  */
-char *_putchar(va_list c, __attribute__((unused)) printing_format * format)
+int _putchar(char c)
 {
-	char a = va_arg(c, int), *s = malloc(sizeof(char) * 2);
-
-	if (!s)
-		return (NULL);
-	s[0] = a;
-	s[1] = '\0';
-
-	return (s);
-}
-
-/**
- * _putstr - writes the character c to stdout
- * @c: an arg list containing the string to be printed
- * @format: the format to use to prepare the string
- * Return: On success the lenght of the string printed
- * -1 on failure
- */
-char *_putstr(va_list c, __attribute__((unused))printing_format * format)
-{
-	char *s = va_arg(c, char *), *s2 = malloc(sizeof(char) * _strlen(s) + 1);
-
-	if (!s2 || !s)
-		return (NULL);
-	_strcpy(s2, s);
-	return (s2);
+write(1, &c, 1);
+return (1);
 }
